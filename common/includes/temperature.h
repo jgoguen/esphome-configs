c94acf39d5381a0d9c0776c4ef045f1432da6d2f
Joel Goguen <jgoguen@users.noreply.github.com>
2021-04-01 19:38:41 -0700

    Updates to use packages structure instead of multiple merges

--- common/includes/temperature.h
+++
@@ -1,9 +0,0 @@
-// vim: filetype=cpp:noexpandtab:sw=2:sts=2:ts=2:nospell
-
-static const float ctof_factor = 9.0 / 5.0;
-
-float celsius_to_fahrenheit(float celsius_temperature);
-
-float celsius_to_fahrenheit(float celsius_temperature) {
-	return (celsius_temperature * ctof_factor) + 32.0;
-}
